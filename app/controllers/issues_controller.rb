5d00fa4bf885e5f821a8424008a704ef0a92b0b3
imears <maxwellmears@gmail.com>
2015-04-06 05:15:03 -0700

    Add ability to create an issue and display it

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -6,4 +6,20 @@ class IssuesController < ApplicationController
   def new
     @issue = Issue.new
   end
+
+  def create
+    if @issue = Issue.create(issue_params)
+      current_user.issues << @issue
+      redirect_to user_issues_path(current_user)
+    else
+      flash[:error] = "didnt work!!"
+      render 'new'
+    end
+  end
+
+  private
+
+    def issue_params
+      params.require(:issue).permit(:subject, :priority, :description)
+    end
 end
