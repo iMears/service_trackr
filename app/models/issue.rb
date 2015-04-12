8199db568614687c50d7d27e7bd8fbaa48cedb6f
imears <maxwellmears@gmail.com>
2015-04-11 19:08:57 -0700

    Add validations to issues

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,8 @@
 class Issue < ActiveRecord::Base
   belongs_to :user
   has_many :entries, dependent: :destroy
+
+  validates :subject, presence: true
+  validates :priority, presence: true
+  validates :description, presence: true
 end
