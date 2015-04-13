a9873341c0eab5ae4702154d8f43525396c50664
imears <maxwellmears@gmail.com>
2015-04-13 02:07:58 -0700

    Validate picture size for entries model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,13 @@ class Entry < ActiveRecord::Base
   belongs_to :issue
 
   mount_uploader :picture, PictureUploader
+
+  validate :picture_size
+
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
