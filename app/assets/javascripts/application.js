806b5cef7a56ec3c37e87a28ce5f0541b85bdb99
imears <maxwellmears@gmail.com>
2015-04-14 23:48:28 -0700

    Move javascript from view to application.js where is should be :wink:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,4 +31,11 @@ $(document).ready(function() {
     });
   });
 
+  $(document).bind('change', '#entry_picture', function() {
+    sizeInMegabytes = this.files[0].size/1024/1024;
+    if (sizeInMegabytes > 5) {
+      alert('Maximum file size is 5MB. Please choose a smaller file!');
+    }
+  });
+
 });
