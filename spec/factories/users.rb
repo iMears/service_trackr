dca51c43a46ca1bc0ce07816236394f676d9bc69
imears <maxwellmears@gmail.com>
2015-04-15 01:37:15 -0700

    Add factory for user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
-FactoryGirl.define do
-  factory :user do
-    
-  end
+require 'faker'
 
+FactoryGirl.define do
+  factory :user do |f|
+    f.first_name { Faker::Name.first_name }
+    f.last_name { Faker::Name.last_name }
+    f.email { Faker::Internet.email }
+    f.password {Faker::Internet.password(8) }
+    f.company { Faker::Company.name }
+  end
 end
