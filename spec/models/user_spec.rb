bbca1d442516ec86d6539ead61375cd0a394e27c
imears <maxwellmears@gmail.com>
2015-04-15 01:38:00 -0700

    Add basic tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    FactoryGirl.create(:user).should be_valid
+  end
+
+  it 'is invalid without a first name' do
+    FactoryGirl.build(:user, first_name: nil).should_not be_valid
+  end
+
+  it 'is invalid without a last name' do
+    FactoryGirl.build(:user, last_name: nil).should_not be_valid
+  end
+
+  it 'is invalid without a company' do
+    FactoryGirl.build(:user, company: nil).should_not be_valid
+  end
+
+  it 'is invalid without a email' do
+    FactoryGirl.build(:user, email: nil).should_not be_valid
+  end
+
+  it 'does not allow duplicate emails' do
+    FactoryGirl.create(:user, email: 'john@example.com')
+    FactoryGirl.build(:user, email: 'john@example.com').should_not be_valid
+  end
 end
