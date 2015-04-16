fd9612053a4755c80a7271fca80d37f36b3cc194
imears <maxwellmears@gmail.com>
2015-04-15 23:23:29 -0700

    Add specs for entries model

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is a valid entry' do
+    expect(FactoryGirl.create(:entry)).to be_valid
+  end
+
+  it 'is invalid without a posted by' do
+    expect(FactoryGirl.build(:entry, posted_by: nil)).to be_invalid
+  end
+
+  it 'is invalid without a body' do
+    expect(FactoryGirl.build(:entry, body: nil)).to be_invalid
+  end
 end
