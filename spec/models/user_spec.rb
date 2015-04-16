5338691e061c7576d85a554ecfe7a88b2e91fa27
imears <maxwellmears@gmail.com>
2015-04-15 23:22:46 -0700

    Adjust user spec to used expect instead of should

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,27 +2,27 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'has a valid factory' do
-    FactoryGirl.create(:user).should be_valid
+    expect(FactoryGirl.create(:user)).to be_valid
   end
 
   it 'is invalid without a first name' do
-    FactoryGirl.build(:user, first_name: nil).should_not be_valid
+    expect(FactoryGirl.build(:user, first_name: nil)).to be_invalid
   end
 
   it 'is invalid without a last name' do
-    FactoryGirl.build(:user, last_name: nil).should_not be_valid
+    expect(FactoryGirl.build(:user, last_name: nil)).to be_invalid
   end
 
   it 'is invalid without a company' do
-    FactoryGirl.build(:user, company: nil).should_not be_valid
+    expect(FactoryGirl.build(:user, company: nil)).to be_invalid
   end
 
   it 'is invalid without a email' do
-    FactoryGirl.build(:user, email: nil).should_not be_valid
+    expect(FactoryGirl.build(:user, email: nil)).to be_invalid
   end
 
   it 'does not allow duplicate emails' do
     FactoryGirl.create(:user, email: 'john@example.com')
-    FactoryGirl.build(:user, email: 'john@example.com').should_not be_valid
+    expect(FactoryGirl.build(:user, email: 'john@example.com')).to be_invalid
   end
 end
