fe0c49fee91aad9ba9ddba011e06c49163ab13c6
imears <maxwellmears@gmail.com>
2015-04-16 22:36:24 -0700

    Update spec to not use old syntax for FactoryGirl

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'has a valid factory' do
-    expect(FactoryGirl.create(:user)).to be_valid
+  describe 'validations' do
+    it 'has a valid factory' do
+      expect(create(:user)).to be_valid
+    end
+
+    it 'is invalid without a first name' do
+      expect(build(:user, first_name: nil)).to be_invalid
+    end
+
+    it 'is invalid without a last name' do
+      expect(build(:user, last_name: nil)).to be_invalid
+    end
+
+    it 'is invalid without a company' do
+      expect(build(:user, company: nil)).to be_invalid
+    end
+
+    it 'is invalid without a email' do
+      expect(build(:user, email: nil)).to be_invalid
+    end
+
+    it 'does not allow duplicate emails' do
+      create(:user, email: 'john@example.com')
+      expect(build(:user, email: 'john@example.com')).to be_invalid
+    end
   end
 
-  it 'is invalid without a first name' do
-    expect(FactoryGirl.build(:user, first_name: nil)).to be_invalid
-  end
+  describe '#some_method' do
 
-  it 'is invalid without a last name' do
-    expect(FactoryGirl.build(:user, last_name: nil)).to be_invalid
-  end
-
-  it 'is invalid without a company' do
-    expect(FactoryGirl.build(:user, company: nil)).to be_invalid
-  end
-
-  it 'is invalid without a email' do
-    expect(FactoryGirl.build(:user, email: nil)).to be_invalid
-  end
-
-  it 'does not allow duplicate emails' do
-    FactoryGirl.create(:user, email: 'john@example.com')
-    expect(FactoryGirl.build(:user, email: 'john@example.com')).to be_invalid
   end
 end
