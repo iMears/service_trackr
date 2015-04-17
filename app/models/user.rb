dee701e84d8fc442c366328794b1fe3500f72a60
imears <maxwellmears@gmail.com>
2015-04-17 00:49:40 -0700

    Add tests for validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :issues, dependent: :destroy
 
-  validates :first_name, presence: true, length: { minimum: 2 }
-  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :first_name, presence: true, length: { minimum: 2 }, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates :last_name, presence: true, length: { minimum: 2 }, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   validates :company, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,16 +4,22 @@ RSpec.describe User, type: :model do
   let(:some_user) { build(:user) }
 
   describe "ActiveModel validations" do
+
+    # Basic validations
     it { expect(some_user).to validate_presence_of(:first_name) }
     it { expect(some_user).to validate_presence_of(:last_name) }
     it { expect(some_user).to validate_presence_of(:company) }
     it { expect(some_user).to validate_presence_of(:email) }
     it { expect(some_user).to validate_uniqueness_of(:email) }
 
-    it 'does not allow duplicate emails' do
-      create(:user, email: 'john@example.com')
-      expect(build(:user, email: 'john@example.com')).to be_invalid
-    end
+    # Format validations
+    it { expect(some_user).to allow_value("Maxwell").for(:first_name) }
+    it { expect(some_user).to allow_value("Mears").for(:last_name) }
+    it { expect(some_user).to_not allow_value("@Maxwell").for(:first_name) }
+    it { expect(some_user).to_not allow_value(" Mears").for(:last_name) }
+    it { expect(some_user).to allow_value("", nil).for(:phone_number) }
 
+    # Inclusion/acceptance of values
+    it { expect(some_user).to validate_confirmation_of(:password) }
   end
 end
