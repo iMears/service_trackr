d9e60241015fe44905416356e54664758de37068
imears <maxwellmears@gmail.com>
2015-04-27 23:59:14 -0700

    Add testes for entry

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  it 'is a valid entry' do
-    expect(FactoryGirl.create(:entry)).to be_valid
-  end
 
-  it 'is invalid without a posted by' do
-    expect(FactoryGirl.build(:entry, posted_by: nil)).to be_invalid
-  end
+  describe "ActiveModel validations" do
 
-  it 'is invalid without a body' do
-    expect(FactoryGirl.build(:entry, body: nil)).to be_invalid
+    # Basic validations
+    it { expect(:entry).to validate_presence_of(:posted_by) }
+    it { expect(:entry).to validate_presence_of(:body) }
+
+    it 'is a valid entry' do
+      expect(FactoryGirl.create(:entry)).to be_valid
+    end
+
+    it 'is invalid without a posted by' do
+      expect(FactoryGirl.build(:entry, posted_by: nil)).to be_invalid
+    end
+
+    it 'is invalid without a body' do
+      expect(FactoryGirl.build(:entry, body: nil)).to be_invalid
+    end
+
   end
 end
